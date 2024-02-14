b6b0b8067fce99e991b1a10bc7fe87a0bddb48cd
edisonospina16 <edison100ospina@gmail.com>
2024-02-13 22:17:11 -0500

    ejercicio #1.py realizado

---
+++ README.md
@@ -0,0 +1,3 @@
+## Actividad de participacion 
+Encontrarás una serie de ejercicios básicos
+de programación para que los implementes en Python.
--- ejecicios/ejercicio #1.py
+++ ejecicios/ejercicio #1.py
@@ -2,3 +2,10 @@
 # ingresar su nombre y edad y los imprima en pantalla.
 
 
+def main():
+    nombre = input("ingresa tu nombre: ")
+    edad = input("ingresa tu edad: ")
+
+if __name__== "__main__":
+    main()
+
