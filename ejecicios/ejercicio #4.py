b61cad299613916cff421370b2a9cb80c09c140d
edisonospina16 <edison100ospina@gmail.com>
2024-02-14 22:06:13 -0500

    ejercicio #4.py completo

---
+++ ejecicios/ejercicio #4.py
@@ -0,0 +1,8 @@
+def es_par_o_impar():
+    num = int(input("ingrese un numero: "))
+    if num % 2 == 0:
+         print(f"el numero:{num} es par")
+    else:
+        print(f"el numero:{num} es impar")
+
+es_par_o_impar()
